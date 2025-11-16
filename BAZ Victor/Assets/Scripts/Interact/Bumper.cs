2c3955cd2638354554214caf58b8c7afc8c1b265
Victor <victor.baz92@gmail.com>
2025-11-16 17:27:16 +0100

    LD

--- BAZ Victor/Assets/Scripts/Interact/Bumper.cs
+++ BAZ Victor/Assets/Scripts/Interact/Bumper.cs
@@ -6,6 +6,7 @@ namespace Interact
     public class Bumper : MonoBehaviour
     {
         [SerializeField] private float strenght;
+        [SerializeField] private bool resetVelocity = false;
         private void OnCollisionEnter(Collision other)
         {
             if (other.gameObject.CompareTag("Hider") || other.gameObject.CompareTag("Seeker"))
@@ -16,6 +17,11 @@ namespace Interact
 
         private void Bump(Rigidbody rb)
         {
+            if (resetVelocity)
+            {
+                rb.angularVelocity = Vector3.zero;
+                rb.linearVelocity = Vector3.zero;
+            }
             rb.AddForce(Vector3.up * strenght ,ForceMode.Impulse);
         }
     }
